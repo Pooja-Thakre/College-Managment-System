3b908b2125cac803b8f4468990d016c1b4dd7130
Pooja-Thakre <poojathakre2006@gmail.com>
2022-10-08 15:15:54 +0530

    Commit

---
+++ College-Management-System/Common/cards.php
@@ -0,0 +1,55 @@
+</section>
+	<section class=" bg-light text-center">
+		<div class=" w-100 pt-5 m-auto" style="
+	width:100%;
+	padding-right:15px;
+	padding-left:15px;
+	margin-right:auto;
+	margin-left:auto;
+">
+			<h1>Departments</h1>
+			<!-- <p>ICBS serves the national and international community through a broad spectrum of undergraduate studies..</p> -->
+			<div class="row pt-3 pb-3">
+				<div class="col-lg-3 col-md-3 col-sm-12 col-10 m-auto">
+					<div class="card card-margin">
+						<div class="card-logo m-auto">
+							<img src="images/a.jpg" class="card-img img-fluid">
+						</div>
+						<div class="card-body">
+							<h5 class="card-title">Computer Engineering</h5>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-12 col-10 m-auto">
+					<div class="card card-margin">
+						<div class="card-logo m-auto mt-1">
+							<img src="images/b.jpg" class="card-img img-fluid">
+						</div>
+						<div class="card-body">
+							<h5 class="card-title">Mechanical Engineering</h5>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-12 col-10 m-auto">
+					<div class="card card-margin">
+						<div class="card-logo m-auto">
+							<img src="images/c.jpeg" class="card-img img-fluid">
+						</div>
+						<div class="card-body">
+							<h5 class="card-title">Electrical Engineering</h5>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-12 col-10 m-auto">
+					<div class="card card-margin">
+						<div class="card-logo m-auto">
+							<img src="images/d.jpg" class="card-img img-fluid">
+						</div>
+						<div class="card-body">
+							<h5 class="card-title">Electronics and Telecommunication Engineering</h5>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>	
+	</section>
